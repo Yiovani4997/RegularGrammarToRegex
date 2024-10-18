4a4cf4e5730dffa3b7465ab18738e65325fad02e
Yiovani4997 <152087865+Yiovani4997@users.noreply.github.com>
2024-10-18 18:20:17 +0000

    correccion de error de lectura

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 int main(){
-    FILE *grammar = fopen("gramatica1.txt", "r");
+    FILE *grammar = fopen("gramatica.txt", "r");
     if(grammar == NULL){
         perror("Error openning the file");
         return EXIT_FAILURE;
@@ -19,24 +19,29 @@ int main(){
     return EXIT_SUCCESS;
 
 }
-Node *createNode(const char *line){
+Node *createNode(const char *ruleIdentifier, const char *production){
 
     Node *newNode = (Node *)malloc(sizeof(Node));
-    newNode->line = strdup(line);
+    newNode->ruleIdentifier = strdup(ruleIdentifier);
+    newNode->production = strdup(production);
     newNode->next = NULL;
 
-    return (newNode);
+    return newNode;
 }
-void appendNode(Node **head, const char *line){
-    Node *newNode = createNode(line);
+void appendNode(Node **head, const char *ruleIdentifier, const char *production){
+    Node *newNode = createNode(ruleIdentifier, production);
 
     if(*head == NULL){
-        //Empty list
+        *head = newNode;
     }
     else{
-        //At 
+        Node *current = *head;
+        while(current->next != NULL){
+            current = current->next; 
+        }
+        current->next = newNode;
     }
-
+    
 }
 void freeLinkedList(Node *head){
 
@@ -45,15 +50,28 @@ void freeLinkedList(Node *head){
 Node* createLinkedList(FILE *file){
     Node *head = NULL; //Head of the linked list
     char line[MAX_LINE_LENGHT];
+    char ruleIdentifier[MAX_LINE_LENGHT]; 
+    char production[MAX_LINE_LENGHT]; 
 
     while(fgets(line, sizeof(line), file)){
         //remove the newline character if present
         line[strcspn(line, "\n")] = '\0';
-        appendNode(&head, line);
+        
+        const char *delimiter = strstr(line, "->"); 
+        if (delimiter != NULL){
+            strncpy(ruleIdentifier, line, delimiter - line); 
+            ruleIdentifier[delimiter - line] = '\0'; 
+            strcpy(production, delimiter + 2); 
+            appendNode(&head, ruleIdentifier, production);
+        }
     }
 
     return (head);
 }
 void printList(Node *head){
-    
+    Node *current = head;
+    while(current != NULL){
+        printf("%s->%s\n", current->ruleIdentifier, current->production);
+        current = current->next;
+    }
 }
